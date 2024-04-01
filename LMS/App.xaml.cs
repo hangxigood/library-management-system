52010ba1129a221aa01484c1bf9feaa36170e72d
hangxigood <hangxigood@gmail.com>
2024-04-01 10:11:02 -0600

    first edition(SQLite-net)

---
+++ LMS/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LMS;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ LMS/obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("LMS.MainApplication, LMS, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc64dc0d8ce28b900c95.MainApplication.class, crc64dc0d8ce28b900c95.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
